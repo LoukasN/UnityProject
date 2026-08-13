15e83088f9e65144ed2c5602988c88ea9d867c84
LoukasN <lounikus@gmail.com>
2026-08-13 18:12:06 +0300

    Dynamic loader

--- Assets/Scripts/ScenarioLoader.cs
+++ Assets/Scripts/ScenarioLoader.cs
@@ -3,49 +3,37 @@ using System.IO;
 using Newtonsoft.Json;
 
 public class ScenarioLoader : MonoBehaviour {
-    private Scenario scenario;
+    private Scenario currentScenario;
 
     void Start() {
         Debug.Log("Starting Scenario...");
-        string path = Path.Combine(Application.streamingAssetsPath, "decisionTree.json");
-        
+        string[] filePath = GetScenarios();
+        if (filePath.Length > 0){
+            LoadScenario(filePath[0]);
+        }
+    }
+
+    public string[] GetScenarios() {
+        return Directory.GetFiles(Application.streamingAssetsPath, "*.json");
+    }
+
+    public bool LoadScenario(string path) {
         if (!File.Exists(path)) {
-            Debug.LogError("file not found: " + path);
-            return;
+            Debug.LogError("File not found: " + path);
+            return false;
         }
-
-        string text = File.ReadAllText(path);
-        scenario = JsonConvert.DeserializeObject<Scenario>(text);
-
-        Debug.Log($"Loaded scenario: {scenario.meta.title}");
-        Debug.Log($"Schema version: {scenario.schemaVersion}");
-        Debug.Log($"Initial Hr: {scenario.initialState.vitals.hr}");
-        Debug.Log($"Initial SpO2: {scenario.initialState.vitals.spo2}");
-        Debug.Log($"Initial RR: {scenario.initialState.vitals.rr}");
-        Debug.Log($"Initial Bp: {scenario.initialState.vitals.bp}");
-        Debug.Log($"Initial temp: {scenario.initialState.vitals.temp}");
-        Debug.Log($"Nodes count: {scenario.nodes.Count}");
-        Debug.Log($"Test: {scenario.nodes[0].id}");
-        foreach (var node in scenario.nodes) {
-            if (node.id == "n7_gate_documentation_2") {
-                Debug.Log($"From Id: {node.gateRequirements.requiredForms[0].formId}");
-                foreach (var form in node.gateRequirements.requiredForms[0].fields) {
-                    Debug.Log($"From fields: {form}");
-                }
-            }
+    
+        try {
+            string text = File.ReadAllText(path);
+            currentScenario = JsonConvert.DeserializeObject<Scenario>(text);
+    
+            Debug.Log($"Loaded scenario: {currentScenario.meta.title}");
+            Debug.Log($"Schema version: {currentScenario.schemaVersion}");
+            return true;
         }
-
-        if (scenario.rules.globalRules[0].effects[0].type == "ui_visual"){
-            Debug.Log($"{scenario.rules.globalRules[0].effects[0].type}");
-            Debug.Log($"{scenario.rules.globalRules[0].effects[0].target}");
-            Debug.Log($"{scenario.rules.globalRules[0].effects[0].state}");
+        catch (JsonException ex) {
+            Debug.LogError($"Failed to parse {path}: {ex.Message}");
+            return false;
         }
-
-        if (scenario.rules.globalRules[0].effects[1].type == "ui_toast"){
-            Debug.Log($"{scenario.rules.globalRules[0].effects[1].type}");
-            Debug.Log($"{scenario.rules.globalRules[0].effects[1].style}");
-            Debug.Log($"{scenario.rules.globalRules[0].effects[1].message}");
-        }
-        
     }
 }
