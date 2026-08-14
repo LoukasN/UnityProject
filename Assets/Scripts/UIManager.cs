d9607524c5106b1764adecc446372488e431409f
Yellow_bello <yeetmqst3r@gmail.com>
2026-08-14 21:31:54 +0300

    Added simple start screen with information from json

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,11 @@ public class UIManager : MonoBehaviour
     public GameObject vitalsMonitorPanel;
     public GameObject EHRPanel;
 
+    public GameObject startScreenPanel;
+    public TextMeshProUGUI startTitleText;
+    public TextMeshProUGUI startDescriptionText;
+    public TextMeshProUGUI startGoalsText;
+
     public GameObject toastPanel;
     public TextMeshProUGUI toastText;
     public float toastDuration = 5f;
@@ -23,7 +28,10 @@ public class UIManager : MonoBehaviour
     {
         playerMovement = FindFirstObjectByType<PlayerMovement>();
 
+        OpenStartScreen();
+
         StartCoroutine(PushInitialVitalsWhenReady());
+        StartCoroutine(PopulateStartScreenWhenReady());
     }
 
     IEnumerator PushInitialVitalsWhenReady()
@@ -35,6 +43,20 @@ public class UIManager : MonoBehaviour
             yield return null;
 
         vitalsDataSource.UpdateVitals(scenarioLoader.CurrentScenario.initialState.vitals);
+    }
+
+    IEnumerator PopulateStartScreenWhenReady()
+    {
+        var scenarioLoader = FindFirstObjectByType<ScenarioLoader>();
+
+        while (scenarioLoader.CurrentScenario == null)
+            yield return null;
+
+        var meta = scenarioLoader.CurrentScenario.meta;
+
+        startTitleText.text = meta.title;
+        startDescriptionText.text = meta.description;
+        startGoalsText.text = string.Join("\n", meta.learningGoals);
     }
 
     void Awake()
@@ -123,6 +145,27 @@ public class UIManager : MonoBehaviour
 
 
         playerMovement.canMove = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OpenStartScreen()
+    {
+        startScreenPanel.SetActive(true);
+
+        playerMovement.canMove = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void CloseStartScreen()
+    {
+        startScreenPanel.SetActive(false);
+
+        playerMovement.canMove = true;
+        GameTimer.Instance.StartTimer();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
