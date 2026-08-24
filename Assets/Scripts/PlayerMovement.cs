99a30159ec2908089ea52db95fea8bdfeb3dcb34
Yellow_bello <yeetmqst3r@gmail.com>
2026-08-24 19:49:04 +0300

    Fixes for build

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,8 +18,6 @@ public class PlayerMovement : MonoBehaviour {
 
     void Start() {
         characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     void Update() {
---
+++ Assets/Scripts/SceneBootstrap.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBootstrap : MonoBehaviour {
+    [SerializeField]
+    List<string> scenesToLoad = new List<string>();
+
+    void Awake() {
+        foreach (var sceneName in scenesToLoad) {
+            if (string.IsNullOrWhiteSpace(sceneName)) {
+                continue;
+            }
+
+            if (SceneManager.GetSceneByName(sceneName).isLoaded) {
+                continue;
+            }
+
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+    }
+}
