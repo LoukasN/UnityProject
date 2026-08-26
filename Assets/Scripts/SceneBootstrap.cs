0c40ac3a637ef128dae7ecc91a4547e61391b6df
Yellow_bello <yeetmqst3r@gmail.com>
2026-08-26 19:36:13 +0300

    Resolution Fix ,perma 1080p,windowed

--- Assets/Scripts/SceneBootstrap.cs
+++ Assets/Scripts/SceneBootstrap.cs
@@ -2,17 +2,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneBootstrap : MonoBehaviour {
+public class SceneBootstrap : MonoBehaviour
+{
     [SerializeField]
     List<string> scenesToLoad = new List<string>();
 
-    void Awake() {
-        foreach (var sceneName in scenesToLoad) {
-            if (string.IsNullOrWhiteSpace(sceneName)) {
+    void Awake()
+    {
+        Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+
+        foreach (var sceneName in scenesToLoad)
+        {
+            if (string.IsNullOrWhiteSpace(sceneName))
+            {
                 continue;
             }
 
-            if (SceneManager.GetSceneByName(sceneName).isLoaded) {
+            if (SceneManager.GetSceneByName(sceneName).isLoaded)
+            {
                 continue;
             }
 
